ae863f7c8c2ad995be3bdc251c165d5c979641ed
NatName <natname69@gmail.com>
2020-08-22 17:16:51 +0300

    aws lambda

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,47 @@
-import Express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
+// import Express from 'express';
+// import { ApolloServer } from 'apollo-server-express';
+// import { buildSchema } from 'type-graphql';
 import { connect } from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server-lambda';
+
 import 'reflect-metadata';
 
 import { TaskResolver } from './resolvers/taskResolver';
 
-(async() => {
-  const schema = await buildSchema({
-    resolvers: [ TaskResolver ],
-    emitSchemaFile: true,
-    validate: false,
-  });
-
-  const mongoose = await connect('mongodb+srv://NatName:TaskToDo11@cluster0.qmjx0.mongodb.net/ToDoList?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  });
-
-  await mongoose.connection;
-
-  const server = new ApolloServer({schema});
-
-  const app = Express();
-
-  server.applyMiddleware({app});
-
-  const port = +process.env.PORT || 3000;
-  app.listen({ port }, () =>
-    console.log(`Server running on port ${port}`)
-  );
-})().catch((err) => {
-  console.log(err, 'error');
+connect('mongodb+srv://NatName:TaskToDo11@cluster0.qmjx0.mongodb.net/ToDoList?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
+const typeDefs = gql`
+  type Mutation {
+    createTask(data: TaskInput!): Task!
+    deleteTask(id: String!): Boolean!
+    updateTaskStatus(id: String!): Task!
+  }
+
+  type Query {
+    Task(id: String!): Task!
+    Tasks: [Task!]!
+  }
+
+  enum statusEnum {
+    Done
+    ToDo
+  }
+
+  type Task {
+    id: ID!
+    name: String!
+    status: String!
+  }
+
+  input TaskInput {
+    name: String!
+    status: statusEnum!
+  }
+`;
+
+const server = new ApolloServer({ typeDefs, resolvers: [ TaskResolver ] });
+
+exports.handler = server.createHandler();
